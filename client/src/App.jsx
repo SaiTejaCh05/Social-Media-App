dd22eebd18fa118cc1dcb840b7aa39ec2c4e0f8a
Sai Teja <tejach4963@gmail.com>
2025-12-20 01:51:35 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/feed'
+import Messages from './pages/messages'
+import ChatBox from './pages/chatbox'
+import Connections from './pages/connections'
+import Discover from './pages/discover'
+import Profile from './pages/profile'
+import CreatePost from './pages/CreatePost'
+import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/layout'
+import {Toaster} from 'react-hot-toast'
+
+const App = () => {
+  const {user}=useUser()
+  return (
+    <>
+      <Toaster />
+      <Routes>
+        <Route path='/' element={ !user ? <Login/> : <Layout/>}>
+          <Route index element={<Feed/>}/>
+          <Route path='messages' element={<Messages/>}/>
+          <Route path='messages/:userId' element={<ChatBox/>}/>
+          <Route path='connections' element={<Connections/>}/>
+          <Route path='discover' element={<Discover/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          <Route path='profile/:profileId' element={<Profile/>}/>
+          <Route path='create-post' element={<CreatePost/>}/>
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
